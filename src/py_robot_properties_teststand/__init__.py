51842365d16768a9f358d7ea2298b4b0dbed2598
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2020-11-03 17:13:48 +0100

    Packaging ROS2/ament

--- src/py_robot_properties_teststand/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-@package py_robot_properties_teststand
-@author Maximilien Naveau (maximilien.naveau@gmail.com)
-@license License BSD-3-Clause
-@copyright Copyright (c) 2019, New York University and Max Planck Gesellschaft.
-@date 2019-05-22
-@brief Define the python interface of the robot model using pinocchio and python
-"""
